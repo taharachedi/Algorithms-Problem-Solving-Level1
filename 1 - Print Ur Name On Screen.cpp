950bc9d04a4af223c80fb939d9e9005f834568f2
Taha Rachedi <168340845+taharachedi@users.noreply.github.com>
2024-11-01 20:22:57 +0100

    Add files via upload

---
+++ 1 - Print Ur Name On Screen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Print_Name(string Name)
+{
+    cout << "Ur Name is : " << Name << endl;
+}
+
+int main()
+{
+    Print_Name("Taha");
+
+
+    system("color 6F");
+    system("pause>0");
+    return 0;
+}
